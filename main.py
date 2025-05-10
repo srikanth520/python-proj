d58c7145acddbd569c09d65c11d81b04717c62bb
Sreekanth_Varma <43869795+srikanth520@users.noreply.github.com>
2025-05-10 12:41:42 +0530

    Update main.py
    removed unnecessary code

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ from global_constants import DB_NAME
 if not os.path.exists(DB_NAME):
     sys.exit("ERROR: No database detected. Please execute setup.py first!")
 
-from mgr.mgr_voices import live_speech
-from peewee import *
-
-
 
 def main():
     # Start Voice Recognition
